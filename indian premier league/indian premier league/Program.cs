99e3f3f41f460ce760bce6dd82f64d9fc8e93de9
akash <akashasr9999@gmail.com>
2021-10-03 09:09:34 -0700

    premier league

---
+++ indian premier league/indian premier league/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace indian_premier_league
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
